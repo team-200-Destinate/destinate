2d388a5f5c554fbd9defad128d4dda8c2a73519b
saleh2001k <saleh.ayman2001@gmail.com>
2023-05-29 10:27:21 +0300

    frontend01

---
+++ frontend/src/layout/Header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/navBar';
+
+import './style.scss';
+
+function Header() {
+  return (
+    <header className="main-header">
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
---
+++ frontend/src/pages/home/sections/news/news.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+import './news.scss';
+
+function News() {
+
+  return (
+    <section className='news-section'>
+
+    </section>
+  )
+}
+
+export default News
