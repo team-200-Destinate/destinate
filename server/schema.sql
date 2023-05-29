d19f84b1ce6835c8852000ad01fc7bcf688d9ac9
Laith-Vlad <laithxfg13@gmail.com>
2023-05-29 11:26:33 +0000

    backendfull

--- server/schema.sql
+++ server/schema.sql
@@ -1,8 +1,13 @@
-DROP TABLE IF EXISTS destinate_db;
-
-CREATE DATABASE destinate_db;
+DROP TABLE IF EXISTS hotel_confirmations;
+DROP TABLE IF EXISTS flight_confirmations;
 
 CREATE TABLE flight_confirmations (
   id SERIAL PRIMARY KEY,
   flight_data JSONB
+);
+
+CREATE TABLE hotel_confirmations (
+  id SERIAL PRIMARY KEY,
+  hotel_data JSONB,
+  flight_id INTEGER REFERENCES flight_confirmations (id)
 );
