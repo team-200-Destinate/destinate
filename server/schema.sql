3e907f3aed6dd15e720bd64a4ccef1fb943d8cf7
Laith-Vlad <laithxfg13@gmail.com>
2023-05-30 09:07:50 +0000

    backend4

--- server/schema.sql
+++ server/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS hotel_confirmations;
 DROP TABLE IF EXISTS flight_confirmations;
-
+DROP TABLE IF EXISTS result_confirmations;
 CREATE TABLE flight_confirmations (
   id SERIAL PRIMARY KEY,
   flight_data JSONB
@@ -10,4 +10,12 @@ CREATE TABLE hotel_confirmations (
   id SERIAL PRIMARY KEY,
   hotel_data JSONB,
   flight_id INTEGER REFERENCES flight_confirmations (id)
+);
+CREATE TABLE result_confirmations (
+  id SERIAL PRIMARY KEY,
+  flight_id INTEGER REFERENCES flight_confirmations (id),
+  hotel_price DECIMAL(10,2),
+  hotel_name TEXT,
+  hotel_photo_url TEXT,
+  hotel_rating DECIMAL(3,1)
 );
