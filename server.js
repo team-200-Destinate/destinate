b3d54aef82cf1b9fa9854f146437ab393c69e729
Laith-Vlad <laithxfg13@gmail.com>
2023-05-31 00:41:20 +0000

    cors2

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3007');
+ 
   next();
 });
 const amadeus = new Amadeus({
