6d8e508ddaa09e2e179e330d9a8efb75450a448d
Laith-Vlad <laithxfg13@gmail.com>
2023-06-01 20:46:57 +0000

    testing

--- server.js
+++ server.js
@@ -22,12 +22,14 @@ app.use(bodyParser.json());
 app.use(cors({
   origin: 'http://localhost:4200',
 }));
-
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:4200'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
- 
   next();
 });
 const amadeus = new Amadeus({
