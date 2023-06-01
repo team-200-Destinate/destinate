da17b910f3a70adf366247c2b671391d5fe583ec
Laith-Vlad <laithxfg13@gmail.com>
2023-06-01 20:37:18 +0000

    testing

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
  
